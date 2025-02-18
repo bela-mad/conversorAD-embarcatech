cbd67d12bb28fb43a24684f01f36b675d3e466b7
bela-mad <isabelaargolo3110@gmail.com>
2025-02-18 00:11:47 -0300

    Configurados arquivos .c e .h dos botões com função de init

---
+++ include/button.h
@@ -0,0 +1,14 @@
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#include "pico/stdio.h"
+#include "pico/stdlib.h"
+
+// Defines
+#define BUTTON_A_PIN 5
+#define SW 22
+
+// Cabeçalhos da função
+void button_init(uint gpio);
+
+#endif
